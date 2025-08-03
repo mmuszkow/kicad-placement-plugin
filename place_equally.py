eb297ebaf888c98a08b9021e5613514533820a0d
Maciek Muszkowski <maciek.muszkowski@cern.ch>
2025-08-03 15:19:18 +0200

    created Board class

--- place_equally.py
+++ place_equally.py
@@ -19,84 +19,50 @@ class Footprint:
         self.pos = pcbnew.VECTOR2I(fp.GetPosition())
         fbbox = fp.GetBoundingBox()
         self.bbox = pcbnew.BOX2I(fbbox.GetPosition(), fbbox.GetSize())
+        self.layer = fp.GetLayer()
 
         # bring back the defaults
         fp.Reference().SetVisible(ref_visible)
         fp.Value().SetVisible(val_visible)
         fp.InvalidateGeometryCaches()
 
-class Solution:
-    def __init__(self):
-        self.edge_cuts = []
-        self.bbox = None
-        self.front = []
-        self.back = []
+    def is_valid_placement(self, board):
+        if self.is_ignored:
+            return True
 
-    def randomize_all(self):
-        pass
-
-    def create_new(self):
-        pass
-
-    def is_valid(self):
-        if len(self.edge_cuts) == 0 or self.bbox == None:
+        if not board.bbox.Contains(self.bbox):
             return False
 
-        if len(self.front) == 0 and len(self.back) == 0:
-            return False
+        other = []
+        if self.layer == pcbnew.F_Cu:
+            other = board.front
+        elif self.layer == pcbnew.B_Cu:
+            other = board.back
 
-        for footprint in self.front:
-            if footprint.is_ignored:
+        for footprint in other:
+            if footprint.ref == self.ref:
                 continue
 
-            if not self.bbox.Contains(footprint.bbox):
-                debug(f'{footprint.ref} not in board bbox')
+            if footprint.bbox.Intersects(self.bbox):
                 return False
-            
-            for footprint2 in self.front:
-                if footprint2.ref == footprint.ref:
-                    continue
-                if footprint.bbox.Intersects(footprint2.bbox):
-                    debug(f'{footprint.ref} intersects {footprint2.ref}')
-                    return False
 
-        for footprint in self.back:
-            if footprint.is_ignored:
-                continue
-
-            if not self.bbox.Contains(footprint.bbox):
-                debug(f'{footprint.ref} not in board bbox')
+            if footprint.bbox.Contains(self.bbox):
                 return False
-            
-            for footprint2 in self.back:
-                if footprint2.ref == footprint.ref:
-                    continue
-                if footprint.bbox.Intersects(footprint2.bbox):
-                    debug(f'{footprint.ref} intersects {footprint2.ref}')
-                    return False
 
         return True
 
-def solution_from_board(board):
-    solution = Solution()
-    ignored_list = ['J1', 'U3', 'J4', 'J3']
-    solution.edge_cuts = [d for d in board.GetDrawings() if isinstance(d, pcbnew.PCB_SHAPE) and d.GetLayer() == pcbnew.Edge_Cuts]
-    
-    min_x = min([s.GetBoundingBox().GetX() for s in solution.edge_cuts])
-    min_y = min([s.GetBoundingBox().GetY() for s in solution.edge_cuts])
-    max_x = max([s.GetBoundingBox().GetX() + s.GetBoundingBox().GetWidth() for s in solution.edge_cuts])
-    max_y = max([s.GetBoundingBox().GetY() + s.GetBoundingBox().GetHeight() for s in solution.edge_cuts])
-    solution.bbox = pcbnew.BOX2I(pcbnew.VECTOR2I(min_x, min_y), pcbnew.VECTOR2I(max_x - min_x, max_y - min_y))
-
-    for footprint in board.GetFootprints():
-        layer = footprint.GetLayer()
-        is_ignored = footprint.GetReference() in ignored_list
-        if layer == pcbnew.F_Cu:
-            solution.front.append(Footprint(footprint, is_ignored))
-        elif layer == pcbnew.B_Cu:
-            solution.back.append(Footprint(footprint, is_ignored))
-
-    return solution
+class Board:
+    def __init__(self, board, ignored_list):
+        self.bbox = board.GetBoardEdgesBoundingBox()
+        self.front = []
+        self.back = []
+        for footprint in board.GetFootprints():
+            layer = footprint.GetLayer()
+            is_ignored = footprint.GetReference() in ignored_list
+            if layer == pcbnew.F_Cu:
+                self.front.append(Footprint(footprint, is_ignored))
+            elif layer == pcbnew.B_Cu:
+                self.back.append(Footprint(footprint, is_ignored))
 
 def delete_tracks_and_vias(board):
     tracks = board.GetTracks()
@@ -113,9 +79,9 @@ class PlaceEquallyPlugin(pcbnew.ActionPlugin):
         self.icon_file_name = ''
 
     def Run(self):
+        ignored_list = ['J1', 'U3', 'J4', 'J3']
         board = pcbnew.GetBoard()
-        solution = solution_from_board(board) 
-        wx.MessageBox(self.name, f'is_valid: {solution.is_valid()}')
+        b = Board(board, ignored_list)
 
 PlaceEquallyPlugin().register()
 
