903a00158c932cedce1e55cca85907d4ef22617d
Maciek Muszkowski <maciek.muszkowski@cern.ch>
2025-08-03 16:19:56 +0200

    randomly place

--- place_equally.py
+++ place_equally.py
@@ -1,4 +1,5 @@
 import pcbnew
+import random
 import wx
 
 def debug(msg):
@@ -33,36 +34,54 @@ class Footprint:
         if not board.bbox.Contains(self.bbox):
             return False
 
-        other = []
-        if self.layer == pcbnew.F_Cu:
-            other = board.front
-        elif self.layer == pcbnew.B_Cu:
-            other = board.back
-
-        for footprint in other:
+        for footprint in board.footprints:
             if footprint.ref == self.ref:
                 continue
 
             if footprint.bbox.Intersects(self.bbox):
                 return False
 
-            if footprint.bbox.Contains(self.bbox):
-                return False
+            #if footprint.bbox.Contains(self.bbox):
+            #    return False
 
         return True
+
+    def apply(self, kc_board):
+        fp = kc_board.FindFootprintByReference(self.ref)
+        if fp is not None:
+            fp.SetPosition(self.pos)
 
 class Board:
     def __init__(self, board, ignored_list):
         self.bbox = board.GetBoardEdgesBoundingBox()
-        self.front = []
-        self.back = []
-        for footprint in board.GetFootprints():
-            layer = footprint.GetLayer()
-            is_ignored = footprint.GetReference() in ignored_list
-            if layer == pcbnew.F_Cu:
-                self.front.append(Footprint(footprint, is_ignored))
-            elif layer == pcbnew.B_Cu:
-                self.back.append(Footprint(footprint, is_ignored))
+        self.footprints = [Footprint(fp, fp.GetReference() in ignored_list) for fp in board.GetFootprints()]
+        self.margin = 100
+
+    def try_move_random(self):
+        eligible = [fp for fp in self.footprints if not fp.is_ignored]
+        if len(eligible) == 0:
+            return False
+
+        fp = random.choice(eligible)
+        min_x = self.bbox.GetX() + self.margin
+        min_y = self.bbox.GetY() + self.margin
+        max_x = min_x + self.bbox.GetWidth() - fp.bbox.GetWidth() - self.margin
+        max_y = min_y + self.bbox.GetHeight() - fp.bbox.GetHeight() - self.margin
+        if max_x <= min_x or max_y <= min_y:
+            return False
+
+        rand_x = random.randint(min_x, max_x)
+        rand_y = random.randint(min_y, max_y)
+        rand_pos = pcbnew.VECTOR2I(rand_x, rand_y)
+        test_bbox = pcbnew.BOX2I(rand_pos, fp.bbox.GetSize())
+        for fp2 in self.footprints:
+            if fp2.ref != fp.ref and test_bbox.Intersects(fp2.bbox):
+                return False
+
+        offset = rand_pos - fp.bbox.GetPosition()
+        fp.bbox = test_bbox
+        fp.pos += offset
+        return True
 
 def delete_tracks_and_vias(board):
     tracks = board.GetTracks()
@@ -80,8 +99,12 @@ class PlaceEquallyPlugin(pcbnew.ActionPlugin):
 
     def Run(self):
         ignored_list = ['J1', 'U3', 'J4', 'J3']
-        board = pcbnew.GetBoard()
-        b = Board(board, ignored_list)
+        kc_board = pcbnew.GetBoard()
+        board = Board(kc_board, ignored_list)
+        for i in range(100):
+            board.try_move_random()
+        for fp in board.footprints:
+            fp.apply(kc_board)
 
 PlaceEquallyPlugin().register()
 
